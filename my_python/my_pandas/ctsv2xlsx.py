e409ce02341c837a43fe9914d3d1e5abc4b7e75c
fanmingyue <fanmingyue@lixiaoyun.com>
2024-02-19 14:53:52 +0800

    修改

--- my_python/my_pandas/ctsv2xlsx.py
+++ my_python/my_pandas/ctsv2xlsx.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 input_file='my_python\\my_pandas\\file\\csv2xlsxtest.csv'
 # input_file='my_python\\my_pandas\\file\\tsv2xlsxtest.tsv'
-input_fil1 = 'my_python\\my_pandas\\file\\ka_result.csv'
-output_fil1 = 'my_python\\my_pandas\\file\\ka_result.xlsx'
+input_fil1 = 'my_python\\my_pandas\\file\\20240201_071118_01423_w8hvn.csv'
+output_fil1 = 'my_python\\my_pandas\\file\\20240201_071118_01423_w8hvn.xlsx'
 # 读取csv或tsv文件
-df = pd.read_csv(input_fil1, delimiter='\t',na_filter=False)  # 如果是tsv文件，将delimiter参数设置为'\t'
+df = pd.read_csv(input_fil1, delimiter=',',na_filter=False)  # 如果是tsv文件，将delimiter参数设置为'\t'
 
 # 将单元格格式设置为字符串类型
 df = df.astype(str)
