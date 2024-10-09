5bfe49234d517d69a352822f0c4162d605f052c0
imfmy <fanmingyue@lixiaoyun.com>
2024-10-09 14:17:57 +0800

    修改

--- my_pyspark/rdd/distinct.py
+++ my_pyspark/rdd/distinct.py
@@ -2,8 +2,10 @@ from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
-sc = SparkContext("local", "PySpark Example")
+sc = SparkContext("local", "Example")
 ####### 初始化 spark、sc #######
-
+rdd1=sc.parallelize([1,1,2,2,2,3],3)
+print(rdd1.glom().collect())
+print(rdd1.distinct(2).glom().collect())
 ####### 关闭 SparkContext #######
 sc.stop()
