c066197ac97706f5823a26cef161ddf2d5df11ff
imfmy <fanmingyue@lixiaoyun.com>
2024-10-16 15:55:36 +0800

    saveAsTextFile

--- my_pyspark/rdd/saveAsTextFile.py
+++ my_pyspark/rdd/saveAsTextFile.py
@@ -6,12 +6,10 @@ sc = SparkContext("local", "PySpark Example")
 ####### 初始化 spark、sc #######
 
 
-# 初始化 SparkContext
-sc = SparkContext("local", "SaveAsTextFileExample")
-
 # 创建一个包含整数的 RDD
 rdd = sc.parallelize([1, 2, 3, 4, 5],2)
-
+rdd.glom().collect()
+# [[1, 2], [3, 4, 5]]
 # 将 RDD 保存为文本文件
 rdd.saveAsTextFile("output/text_output")
 # ll ./output/text_output/
@@ -19,5 +17,11 @@ rdd.saveAsTextFile("output/text_output")
 # -rw-r--r--. 1 atguigu atguigu 4 10月 16 15:35 part-00000
 # -rw-r--r--. 1 atguigu atguigu 6 10月 16 15:35 part-00001
 # -rw-r--r--. 1 atguigu atguigu 0 10月 16 15:35 _SUCCESS
+
+# cat output/text_output/part-00001
+# 3
+# 4
+# 5
+
 ####### 关闭 SparkContext #######
 sc.stop()
