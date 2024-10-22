e7ae2c49920640158f15ad6078e546a70e7d9081
imfmy <imfmy@foxmail.com>
2024-10-22 23:18:39 +0800

    修改
    Signed-off-by: imfmy <imfmy@foxmail.com>

--- my_pyspark/init_sc_spark.py
+++ my_pyspark/init_sc_spark.py
@@ -1,11 +1,9 @@
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 
-spark = SparkSession.builder.getOrCreate()
+spark = SparkSession.builder.appName("Example").config("k1", "v1").getOrCreate()
 sc = SparkContext("local", "PySpark Example")
 ####### 初始化 spark、sc #######
-
-
 
 
 ####### 关闭 SparkContext #######
