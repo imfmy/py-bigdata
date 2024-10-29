1a877c7b5114d8e4ea0e7571028653edf3410778
imfmy <fanmingyue@lixiaoyun.com>
2024-10-29 08:23:59 +0800

    小修改

---
+++ my_pyspark/2_spark_sql/dateframe_reader/json.py
@@ -0,0 +1,24 @@
+from pyspark import SparkContext
+from pyspark.sql.session import SparkSession
+
+spark = SparkSession.builder.appName("Example").config("k1", "v1").getOrCreate()
+sc = SparkContext("local", "PySpark Example")
+####### 初始化 spark、sc #######
+# $cat examples/src/main/resources/people.json
+# {"name":"Michael"}
+# {"name":"Andy", "age":30}
+# {"name":"Justin", "age":19}
+
+df = spark.read.json("examples/src/main/resources/people.json")
+# Displays the content of the DataFrame to stdout
+df.show()
+# +----+-------+
+# | age|   name|
+# +----+-------+
+# |null|Michael|
+# |  30|   Andy|
+# |  19| Justin|
+# +----+-------+
+
+####### 关闭 SparkContext #######
+sc.stop()
