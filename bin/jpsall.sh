b50bcb4ae1c5f85ee2b8a3898ed74814eda50139
imfmy <fanmingyue@lixiaoyun.com>
2025-02-06 18:18:20 +0800

    小修改

--- bin/jpsall.sh
+++ bin/jpsall.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for host in hadoop102 hadoop103 hadoop104
 do
-        echo -e "\033[1;32m==== ssh $host jps|grep -v Jps====\033[0m "
+        echo -e "\033[1;32m==== ssh $host jps|grep -v Jps ====\033[0m"
         ssh $host jps "$@" | grep -v Jps
 done
