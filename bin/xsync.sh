dd1d49951778b23e85b9ed566d6ac3a45970a770
imfmy <fanmingyue@lixiaoyun.com>
2025-02-06 16:30:10 +0800

    小修改

--- bin/xsync.sh
+++ bin/xsync.sh
@@ -8,20 +8,20 @@ fi
 for host in hadoop102 hadoop103 hadoop104; do
   echo ==================== $host ====================
   #3. 遍历所有目录，挨个发送
-  for file in $@; do
+  for file in "$@"; do
     #4. 判断文件是否存在
-    if [ -e $file ]; then
+    if [ -e "$file" ]; then
       #5. 若文件存在则获取父目录绝对路径
       pdir=$(
-        cd -P $(dirname $file)
+        cd -P $(dirname $file) || exit
         pwd
       )
       #6. 获取当前文件的名称
-      fname=$(basename $file)
+      file_name=$(basename "$file")
       #7. 在目的主机创建同名父目录
       ssh $host "mkdir -p $pdir"
       #8. 将本目录文件同步至目的主机同目录中
-      rsync -av $pdir/$fname $host:$pdir
+      rsync -av $pdir/$file_name $host:$pdir
     else
       #9. 若本机文件不存在则提示文件不存在
       echo $file does not exists!
