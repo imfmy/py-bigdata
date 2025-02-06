e5d50b1b4c58e30a09f593c13991c79f32c87634
imfmy <fanmingyue@lixiaoyun.com>
2025-02-06 18:16:25 +0800

    小修改

--- bin/jpsall.sh
+++ bin/jpsall.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 for host in hadoop102 hadoop103 hadoop104
 do
-        echo ===='ssh' $host 'jps|grep -v Jps'====
-        ssh $host jps $@ | grep -v Jps
+        echo -e "\033[1;32m==== ssh $host jps|grep -v Jps====\033[0m"
+        ssh $host jps "$@" | grep -v Jps
 done
--- bin/myhadoop.sh
+++ bin/myhadoop.sh
@@ -1,30 +1,31 @@
 #! /bin/bash
 if [ $# -lt 1 ]; then
-  echo "No Args Input..."
+  echo -e "\033[1;31mNo Args Input...\033[0m"
   exit
 fi
+HADOOP_HOME="/opt/module/hadoop-3.4.1"
+
 case $1 in
 "start")
-  echo " ==== 启动 hadoop集群 ====="
-
-  echo " ---- hadoop102上启动hdfs: sbin/start-dfs.sh----"
-  ssh hadoop102 "/opt/module/hadoop-3.1.3/sbin/start-dfs.sh"
-  echo " ---- hadoop103上启动yarn: start-yarn.sh----"
-  ssh hadoop103 "/opt/module/hadoop-3.1.3/sbin/start-yarn.sh"
-  echo " ---- hadoop102上启动historyserver:mapred --daemon start historyserver---"
-  ssh hadoop102 "/opt/module/hadoop-3.1.3/bin/mapred --daemon start historyserver"
+  echo -e "\033[1;32m==== 启动 hadoop集群 =====\033[0m"
+  echo -e "\033[1;32m---- hadoop102 上启动hdfs: sbin/start-dfs.sh----\033[0m"
+  ssh hadoop102 "${HADOOP_HOME}/sbin/start-dfs.sh"
+  echo -e "\033[1;32m---- hadoop103上启动yarn: start-yarn.sh----\033[0m"
+  ssh hadoop103 "${HADOOP_HOME}/sbin/start-yarn.sh"
+  echo -e "\033[1;32m---- hadoop102上启动historyserver:mapred --daemon start historyserver---\033[0m"
+  ssh hadoop102 "${HADOOP_HOME}/bin/mapred --daemon start historyserver"
   ;;
 "stop")
-  echo " ===== 关闭 hadoop集群 ====="
+  echo -e "\033[1;32m===== 关闭 hadoop集群 =====\033[0m"
 
-  echo " ---- 关闭 historyserver ---------------"
-  ssh hadoop102 "/opt/module/hadoop-3.1.3/bin/mapred --daemon stop historyserver"
-  echo " ---- 关闭 yarn ----"
-  ssh hadoop103 "/opt/module/hadoop-3.1.3/sbin/stop-yarn.sh"
-  echo " ---- 关闭 hdfs ----"
-  ssh hadoop102 "/opt/module/hadoop-3.1.3/sbin/stop-dfs.sh"
+  echo -e "\033[1;32m---- 关闭 historyserver ---------------\033[0m"
+  ssh hadoop102 "${HADOOP_HOME}/bin/mapred --daemon stop historyserver"
+  echo -e "\033[1;32m---- 关闭 yarn ----\033[0m"
+  ssh hadoop103 "${HADOOP_HOME}/sbin/stop-yarn.sh"
+  echo -e "\033[1;32m---- 关闭 hdfs ----\033[0m"
+  ssh hadoop102 "${HADOOP_HOME}/sbin/stop-dfs.sh"
   ;;
 *)
-  echo "Input Args Error..."
+  echo -e "\033[1;31mInput Args Error...\033[0m"
   ;;
 esac
